75b2b2e65b5381e27cc81a9b2d6c4320f4c319f0
matheus-borba <54252575+matheus-borba@users.noreply.github.com>
2019-09-22 15:57:00 -0300

    Add files via upload

---
+++ gestaoGastos/app/src/main/java/com/example/gestaogastos/Cadastro.java
@@ -0,0 +1,82 @@
+package com.example.gestaogastos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.gestaogastos.DBHelper.GastosDb;
+import com.example.gestaogastos.model.Gastos;
+
+public class Cadastro extends AppCompatActivity {
+
+    EditText editText_Valor, editText_Data, editText_TipoGasto, editText_Local;
+    Button btn_Cadastrar;
+    Gastos editarGasto, gasto;
+    GastosDb dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        gasto = new Gastos();
+        dbHelper = new GastosDb(Cadastro.this);
+
+        Intent intent = getIntent();
+        editarGasto = (Gastos) intent.getSerializableExtra("gasto_escolhido");
+
+        editText_Valor = (EditText) findViewById(R.id.editText_Valor);
+        editText_Data = (EditText) findViewById(R.id.editText_Data);
+        editText_TipoGasto = (EditText) findViewById(R.id.editText_TipoGasto);
+        editText_Local = (EditText) findViewById(R.id.editText_Local);
+
+        btn_Cadastrar = (Button) findViewById(R.id.button_Cadastrar);
+
+        if(editarGasto != null){
+            btn_Cadastrar.setText(R.string.modificar);
+
+            editText_Valor.setText(editarGasto.getValor()+"");
+            editText_Data.setText(editarGasto.getData());
+            editText_TipoGasto.setText(editarGasto.getTipo());
+            editText_Local.setText(editarGasto.getLocal());
+
+            gasto.setId(editarGasto.getId());
+
+        }else {
+            btn_Cadastrar.setText(R.string.cadastrar);
+        }
+
+        btn_Cadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                if(editText_Valor.getText().toString().trim().equals("")){
+                    Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG).show();
+                }else {
+
+                    gasto.setValor(Double.parseDouble(editText_Valor.getText().toString()));
+                    gasto.setData(editText_Data.getText().toString());
+                    gasto.setTipo(editText_TipoGasto.getText().toString());
+                    gasto.setLocal(editText_Local.getText().toString());
+
+                    if (btn_Cadastrar.getText().toString().equals("Cadastrar")) {
+                        dbHelper.salvarGastos(gasto);
+                        dbHelper.close();
+                    } else {
+                        dbHelper.alterarGastos(gasto);
+                        dbHelper.close();
+                    }
+
+                    Intent e = new Intent(Cadastro.this, MainActivity.class);
+                    startActivity(e);
+                }
+
+            }
+        });
+    }
+}
